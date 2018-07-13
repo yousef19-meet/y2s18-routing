28b1509035d40e7924e80721c8f1858c376ff7f0
emiliu <emilyliu10@gmail.com>
2018-07-12 23:39:55 -0400

    some basic examples and exercises

---
+++ example2.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('h.html')
+
+@app.route('/hello/<string:name>')
+def hello_route(name):
+    return render_template('hello.html', n=name)
+
+app.run(debug=True)
