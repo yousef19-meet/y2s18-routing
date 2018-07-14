3e3cbe60befb1d4bebb5f784598ad09b226b0b99
emiliu <emilyliu10@gmail.com>
2018-07-14 00:11:53 -0400

    added and edited examples

--- example3.py
+++ example3.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template('home.html')
+def home_route():
+    return render_template('home_with_link.html')
 
 @app.route('/hello/<string:name>')
 def hello_route(name):
-    return render_template('hello.html', n=name)
+    return render_template('hello_with_link.html', n=name)
 
 app.run(debug=True)
