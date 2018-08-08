9b54f30af8ad29a4e78ea22ea776b0fa0b6232f3
yousef19-meet <yousef19@meet.mit.edu>
2018-08-08 04:10:32 -0700

    flask and shit idfk

--- exercises/app.py
+++ exercises/app.py
@@ -2,8 +2,13 @@ from databases import *
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return 'START HERE: CHANGE ME'
+    return render_template('home.html')
+
+@app.route('/student/<int:student_id>')
+def display_student(student_id):
+    return render_template('student.html', student_id=student_id)
 
 app.run(debug=True)
